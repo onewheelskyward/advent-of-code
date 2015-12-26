67e18cd84ee7a8afdc0d2d8dba52d5098b28cc81
Andrew Kreps <andrew.kreps@gmail.com>
2015-12-26 14:34:24 -0700

    Day 1.2!

--- day-1.rb
+++ day-1.rb
@@ -1,14 +1,19 @@
 input = File.read('day-1-input.txt')
 
 floor = 0
+first_basement = false
 
-input.chars.each do |c|
+input.chars.each_with_index do |c, index|
   if '(' == c
     floor += 1
   end
   if ')' == c
     floor -= 1
   end
+  if floor == -1 and not first_basement
+    puts "Day 1.2 First basement index: #{index + 1}"
+    first_basement = true
+  end
 end
 
-puts floor
+puts "Day 1.1 floor: #{floor}"
