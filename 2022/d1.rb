c439a295480da9fdbd20b82f656eb4f01564cb3e
Andrew Kreps <andrew.kreps@gmail.com>
2022-12-01 23:25:14 -0800

    Day 1-1 + 1-2 '22

---
+++ 2022/d1.rb
@@ -0,0 +1,35 @@
+elves = []
+etemp = []
+
+#sep the space-line delimited data into elves' sacks weights in calories
+File.open('d1-data.txt').each do |l|
+  l.strip!
+  if l.to_i > 0
+    etemp.push l.to_i
+  else
+    elves.push etemp
+    etemp = []
+  end
+end
+
+puts elves.inspect
+
+elfcals = []
+
+elves.each do |elf|
+  cals = elf.sum
+  puts cals
+  elfcals.push cals
+end
+
+puts "D1-1 Max cals: #{elfcals.max}"
+
+ec_sorted = elfcals.sort.reverse
+
+puts ec_sorted.inspect
+
+puts "D1-2 Top 3: #{ec_sorted[0]},#{ec_sorted[1]},#{ec_sorted[2]}"
+
+top_three = ec_sorted[0] + ec_sorted[1] + ec_sorted[2]
+
+puts "Top 3 summed: #{top_three}"
